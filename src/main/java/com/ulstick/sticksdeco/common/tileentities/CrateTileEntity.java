c34790f173dc99900b721f44ec8401d40acccc02
ULSTICK <ulstick39@gmail.com>
2022-01-18 20:14:14 +0700

    I Gave up Porting for now... 😪

--- src/main/java/com/ulstick/sticksdeco/common/tileentities/CrateTileEntity.java
+++ src/main/java/com/ulstick/sticksdeco/common/tileentities/CrateTileEntity.java
@@ -17,6 +17,7 @@ import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.ChestMenu;
+import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.ContainerOpenersCounter;
@@ -24,7 +25,9 @@ import net.minecraft.world.level.block.entity.RandomizableContainerBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class CrateTileEntity extends RandomizableContainerBlockEntity {
-    private NonNullList<ItemStack> items = NonNullList.withSize(27, ItemStack.EMPTY);
+
+    private NonNullList<ItemStack> items;
+
     private ContainerOpenersCounter openersCounter = new ContainerOpenersCounter() {
         protected void onOpen(Level worldIn, BlockPos pos, BlockState state) {
             CrateTileEntity.this.playSound(state, SoundEvents.FENCE_GATE_OPEN);
@@ -34,6 +37,7 @@ public class CrateTileEntity extends RandomizableContainerBlockEntity {
         protected void onClose(Level worldIn, BlockPos pos, BlockState state) {
             CrateTileEntity.this.playSound(state, SoundEvents.FENCE_GATE_CLOSE);
             CrateTileEntity.this.updateBlockState(state, false);
+            setChanged();
         }
 
         protected void openerCountChanged(Level worldIn, BlockPos pos, BlockState state, int p_155069_, int p_155070_) {
@@ -51,31 +55,26 @@ public class CrateTileEntity extends RandomizableContainerBlockEntity {
 
     public CrateTileEntity(BlockPos pos, BlockState state) {
         super(ModTileEntity.CRATE_TILE.get(), pos, state);
-    }
-
-    @Override
-    public void load(CompoundTag nbt) {
-        super.load(nbt);
         this.items = NonNullList.withSize(this.getContainerSize(), ItemStack.EMPTY);
-        if (!this.tryLoadLootTable(nbt)) {
-            ContainerHelper.loadAllItems(nbt, this.items);
+    }
+
+    @Override
+    protected void saveAdditional(CompoundTag tag) {
+        super.saveAdditional(tag);
+        if (!this.trySaveLootTable(tag)) {
+            ContainerHelper.saveAllItems(tag, this.items);
         }
     }
 
     @Override
-    public CompoundTag save(CompoundTag nbt) {
-        if (!this.trySaveLootTable(nbt)) {
-            ContainerHelper.saveAllItems(nbt, this.items, true);
+    public void load(CompoundTag tag) {
+        super.load(tag);
+        this.items = NonNullList.withSize(this.getContainerSize(), ItemStack.EMPTY);
+        if (!this.tryLoadLootTable(tag)) {
+            ContainerHelper.loadAllItems(tag, this.items);
         }
-        return nbt;
     }
 
-    //protected void saveAdditional(CompoundTag nbt) {
-    //    super.saveAdditional(nbt);
-    //    if (!this.trySaveLootTable(nbt)) {
-    //        ContainerHelper.saveAllItems(nbt, this.items, true);
-    //    }
-    //}
 
     public int getContainerSize() { return 27; }
 
@@ -85,23 +84,26 @@ public class CrateTileEntity extends RandomizableContainerBlockEntity {
 
     protected Component getDefaultName() { return new TranslatableComponent("container.sticksdeco.crate"); }
 
-    protected AbstractContainerMenu createMenu(int slot, Inventory inventory) {
-        return ChestMenu.threeRows(slot, inventory, this);
+    protected AbstractContainerMenu createMenu(int windowId, Inventory inventory) {
+        return new ChestMenu(MenuType.GENERIC_9x3, windowId, inventory, this, 3);
     }
 
     public void startOpen(Player player) {
+        setChanged();
         if (!this.remove && !player.isSpectator()) {
             this.openersCounter.incrementOpeners(player, this.getLevel(), this.getBlockPos(), this.getBlockState());
         }
     }
 
     public void stopOpen(Player player) {
+        setChanged();
         if (!this.remove && !player.isSpectator()) {
             this.openersCounter.decrementOpeners(player, this.getLevel(), this.getBlockPos(), this.getBlockState());
         }
     }
 
     public void recheckOpen() {
+        setChanged();
         if (!this.remove) {
             this.openersCounter.recheckOpeners(this.getLevel(), this.getBlockPos(), this.getBlockState());
         }
